495df460edfe90c69f111fd42cd3e4ce4507edaa
jeremyherrera <rocafuerteherrera@gmail.com>
2022-09-26 19:56:02 -0500

    como utilizar la funcion

--- app/src/main/java/com/darkbryan/quickstart/MainActivity.kt
+++ app/src/main/java/com/darkbryan/quickstart/MainActivity.kt
@@ -2,18 +2,18 @@ package com.darkbryan.quickstart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.sql.DriverManager.println
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-
-
-
-
-
     }
+
+    fun mostrarHola(){
+        println("Hola KOTLIN")
+    }
+
 }
