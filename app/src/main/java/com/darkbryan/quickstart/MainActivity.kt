5b9a27208510901e0da7db7b8569cb430c227c7a
jeremyherrera <rocafuerteherrera@gmail.com>
2022-09-29 11:35:35 -0500

    Unir el archivo xml con kotlin

--- app/src/main/java/com/darkbryan/quickstart/MainActivity.kt
+++ app/src/main/java/com/darkbryan/quickstart/MainActivity.kt
@@ -1,30 +1,20 @@
-    package com.darkbryan.quickstart
+package com.darkbryan.quickstart
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import java.sql.DriverManager.println
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+    class MainActivity : AppCompatActivity() {
 
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         var x = 40
-         var y = 60
-
-         sumaNumeros(x, y)
+         val textView = findViewById<TextView>(R.id.texto_hola)
 
 
-    }
-    fun sumaNumeros( num1:Int , num2: Int){
-        var suma = num1 + num2
-
-        Log.v("BR","La suma de los numeros es " + suma)
 
 
     }
-
-
 }
