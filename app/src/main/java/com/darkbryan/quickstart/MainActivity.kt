faa0c5cea89ff38d0e07ac44618d96c4fd8c7129
jeremyherrera <rocafuerteherrera@gmail.com>
2022-09-29 01:10:32 -0500

    SE de la suma de numeros de var con fun

--- app/src/main/java/com/darkbryan/quickstart/MainActivity.kt
+++ app/src/main/java/com/darkbryan/quickstart/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.darkbryan.quickstart
+    package com.darkbryan.quickstart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import java.sql.DriverManager.println
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+         var x = 40
+         var y = 60
+
+         sumaNumeros(x, y)
+
+
+    }
+    fun sumaNumeros( num1:Int , num2: Int){
+        var suma = num1 + num2
+
+        Log.v("BR","La suma de los numeros es " + suma)
+
+
     }
 
-    fun mostrarHola(){
-        println("Hola KOTLIN")
-    }
 
 }
