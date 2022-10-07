6a7aefdef9f793ccbd5e995d742978b3faab2f20
jeremyherrera <rocafuerteherrera@gmail.com>
2022-10-07 17:32:52 -0500

    If de Verdadero Else Falso Numero de mayor a menor y igual

--- app/src/main/java/com/darkbryan/quickstart/MainActivity.kt
+++ app/src/main/java/com/darkbryan/quickstart/MainActivity.kt
@@ -12,6 +12,26 @@ import androidx.appcompat.app.AppCompatActivity
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+         val texto = findViewById<TextView>(R.id.texto_bueno) as TextView
+         val numero1: Int = 15
+         val numero2: Int = 10
+
+         if(numero1 != numero2){
+             texto.text = "VERDADERO"
+         }else{
+             texto.text = "FALSO"
+         }
+
+         /*
+         > Mayor que
+         < Menor que
+         >= Mayor o igual
+         <= Menor o igual
+         == igual
+         != distinto
+          */
+
+
 
 
 
