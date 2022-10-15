31e22387efca359facb7e6e7a35940114538674f
jeremyherrera <rocafuerteherrera@gmail.com>
2022-10-15 13:56:52 -0500

    Explicacion de como sirve el parametro

--- app/src/main/java/com/darkbryan/quickstart/MainActivity.kt
+++ app/src/main/java/com/darkbryan/quickstart/MainActivity.kt
@@ -5,39 +5,28 @@ import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+class MainActivity : AppCompatActivity() {
 
-    class MainActivity : AppCompatActivity() {
+    lateinit var  texto : TextView
+    var nombre : String = "BRYA"
 
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         texto = findViewById<TextView>(R.id.texto_bueno) as TextView
 
-         val texto = findViewById<TextView>(R.id.texto_bueno) as TextView
-         val numero1: Int = 15
-         val numero2: Int = 10
-
-         if(numero1 != numero2){
-             texto.text = "VERDADERO"
-         }else{
-             texto.text = "FALSO"
-         }
-
-         /*
-         > Mayor que
-         < Menor que
-         >= Mayor o igual
-         <= Menor o igual
-         == igual
-         != distinto
-          */
+         texto.text = "HOLA COMO ESTAS?"
 
 
-
-
-
-
-
-
+         cambiaText( nombre )
 
     }
+    fun cambiaText( nom : String ){
+        texto.text = nom
+    }
+
+
+
+
+
 }
